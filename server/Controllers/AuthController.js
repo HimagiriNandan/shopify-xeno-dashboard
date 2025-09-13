e8dee275dc183fd5e97bcaf024318d6432332ab3
Himagiri Nandan <himagirinandantuduru@gmail.com>
2025-09-13 23:34:09 +0530

    Enhance userLogin function with additional logging for better debugging

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -37,24 +37,30 @@ export const userSignup = async (req, res) => {
 
 export const userLogin = async (req, res) => {
   const {email, password} = req.body;
+  console.log(`${email} and ${password} are received`);
   try {
     const user = await prisma.user.findUnique({ where: { email } });
     if (!user) {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
+    console.log(user, " user fetched from DB");
+
     const isPsswdVal = await bcrypt.compare(password, user.password);
     if (!isPsswdVal) {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
+    console.log("Password is valid");
 
     const token = createToken(user.username, email, user.id);
     res.cookie('token', token, { 
       httpOnly: true, 
       secure: true 
     });
+    console.log("Token created and cookie set");
     res.status(200).json({ message: 'Login successful', data: {user_id: user.id, username: user.username, email: user.email} });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: 'Server error', error: error.message });
   }
 }
